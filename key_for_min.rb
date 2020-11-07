7cee1f47c436134300efe2fdbabbcf2c8c39863e
Evan Fujita <evanfujita@gmail.com>
2020-11-07 22:49:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,6 @@ def key_for_min_value(name_hash)
   number = 100000
   
   name_hash.each do |key, element|
-     if name_hash[key].include? 
-      nil
    if number > element
       number = element.to_i
     end
