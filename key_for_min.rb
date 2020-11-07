b863ff283f29031844c2504a9769b9862ad05d10
Evan Fujita <evanfujita@gmail.com>
2020-11-07 22:57:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  new_key = 0
+  new_key = nil
   number = 100000
   name_hash.each do |key, element|
   if name_hash[key].class != Integer
